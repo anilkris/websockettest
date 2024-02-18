6d26a1a614487f1350065cfe9d2c7b20d1d2be65
anil <anil@aztik.com>
2024-02-18 11:36:05 +0530

    updated initial code

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'WebSocketClient';
+  stock: any = {};
+
+  private webSocket: WebSocket;
+
+  constructor() {
+    this.webSocket = new WebSocket('ws://localhost:8080/stocks');
+    this.webSocket.onmessage = (event) => {
+      this.stock = JSON.parse(event.data)
+    };
+  } 
+}
